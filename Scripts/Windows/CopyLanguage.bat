cc09cfd84e269d5ac511e43b1b16f64000a1cf28
holdyounger <holdyounger@163.com>
2023-08-04 15:31:35 +0800

    :rocket: 添加脚本文件夹

---
+++ Scripts/Windows/CopyLanguage.bat
@@ -0,0 +1,5 @@
+xcopy /y .\Oem_zh_HK.qm ..\..\resource\language\
+xcopy /y .\Oem_en_US.qm ..\..\resource\language\
+xcopy /y .\Oem_zh_CN.qm ..\..\resource\language\
+
+pause
---
+++ Scripts/Windows/RemoveLogs.bat
@@ -0,0 +1,8 @@
+@echo off
+
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
+REM
+
+rd /Q /S %programdata%\TrustAgent\logs
